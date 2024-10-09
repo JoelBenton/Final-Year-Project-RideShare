75bf117d7d87e8aa0f71630fa1b5869746a905ee
JoelBenton <joelbenton1903@gmail.com>
2024-10-09 16:48:16 +0100

    Removed Unnecessary files

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,9 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
-const userRoutes = require('./src/routes/userRoutes');
 
 const port = process.env.PORT || 3000;
 
 app.use(express.json()); // Middleware to parse JSON bodies
-
-// Routes
-app.use('/user', userRoutes);
 
 app.listen(port, () => console.log(`Server started on port ${port}...`)); 
